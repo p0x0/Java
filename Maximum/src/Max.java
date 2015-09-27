1c38cc823bba008f79d4eb9481adff75f91e552c
Sergey <Megatron90000@gmail.com>
2015-09-27 19:20:05 +0300

    Max java

---
+++ Maximum/src/Max.java
@@ -0,0 +1,23 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+
+public class Max {
+	public static Scanner in = new Scanner(System.in);
+	public static PrintStream out = new PrintStream(System.out);
+	public static void main(String[] args){
+		int a,b,c,max;
+		a= in.nextInt();
+		b= in.nextInt();
+		c= in.nextInt();
+		if (a>b){
+			max = a;
+		}
+		 else{
+			 max = b;
+		}
+
+		if (c>max) max = c;
+		out.println(max);
+	}
+}
